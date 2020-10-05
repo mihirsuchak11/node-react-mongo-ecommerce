03da656d29f61528a5db4a30ea0ad745cfd556bd
Mihir <mihir.suchak11@gmail.com>
2020-10-05 23:49:45 +0530

    Added components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </main>
+      </Container>
+      <Footer />
+    </Router>
   )
 }
 
-export default App
+export default App;
